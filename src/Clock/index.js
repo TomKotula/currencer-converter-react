4bb2de10492a3c88ac8a3e6241636bda6b5aa792
Tom Kotula <test-git@youcode.pl>
2024-03-21 21:00:32 +0100

    clock added

---
+++ src/Clock/index.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import "./clock.css";
+
+const Clock = () => {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className="clock__container">
+      {date.toLocaleString("en-US", 
+      { 
+        weekday: "long",
+        month: "long",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      })}
+    </div>
+  );
+}
+
+export default Clock;
--- src/Header/index.js
+++ src/Header/index.js
@@ -1,6 +1,8 @@
+import Clock from "../Clock";
 
 const Header = () => (
     <header>
+        <Clock/>
         <h1 className="header">Convert your money into foreign currencies</h1>
     </header>
 );
