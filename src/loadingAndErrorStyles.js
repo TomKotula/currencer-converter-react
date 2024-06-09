4b0e0088d1d73b1dfa2d2e5685580efece20369a
Tom Kotula <test-git@youcode.pl>
2024-06-09 16:36:51 +0200

    added styled components for loading and error messages, implemented conditional logic

---
+++ src/loadingAndErrorStyles.js
@@ -0,0 +1,14 @@
+import { styled } from "styled-components";
+
+export const Loading = styled.div`
+  text-align: center;
+  padding: 20px;
+`;
+
+export const Error = styled.div`
+  text-align: center;
+  background-color: red;
+  color: white;
+  padding: 20px;
+  border-radius: 20px;
+`;
