94326e83baae801b3c8c579316c09e694c09f7ae
Tom Kotula <test-git@youcode.pl>
2024-06-09 16:21:56 +0200

    useless code removed

--- src/Options/index.js
+++ src/Options/index.js
@@ -3,14 +3,12 @@ import { SelectContainer, Select } from "./styled";
 const Options = ({ fromCurrency, toCurrency, handleFromCurrencyChange, handleToCurrencyChange }) => (
     <SelectContainer>
         <Select value={fromCurrency} onChange={handleFromCurrencyChange}>
-            <option></option>
             <option>PLN</option>
             <option>EUR</option>
             <option>USD</option>
             <option>CHF</option>
         </Select>
         <Select value={toCurrency} onChange={handleToCurrencyChange}>
-            <option></option>
             <option>PLN</option>
             <option>EUR</option>
             <option>USD</option>
